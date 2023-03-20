d3a7f1cd50f88f94e07eb2995b1205bbc3f210ef
Luan Pham <luanpm88@gmail.com>
2023-03-20 16:01:37 +0700

    prevent seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,28 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Default user for development
-        \App\Models\User::where('email', '=', 'agent@bonnuoclongnhien.com')->delete();
-        \App\Models\User::factory()->create([
-            'name' => 'Agent',
-            'email' => 'agent@bonnuoclongnhien.com',
-            'password' => \Hash::make('123456'),
-        ]);
+        // // Default user for development
+        // \App\Models\User::where('email', '=', 'agent@bonnuoclongnhien.com')->delete();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Agent',
+        //     'email' => 'agent@bonnuoclongnhien.com',
+        //     'password' => \Hash::make('123456'),
+        // ]);
 
-        // Dung Đỗ
-        \App\Models\User::where('email', '=', 'dungdo@bonnuoclongnhien.com')->delete();
-        \App\Models\User::factory()->create([
-            'name' => 'Dung Đỗ',
-            'email' => 'dungdo@bonnuoclongnhien.com',
-            'password' => \Hash::make('123456@'),
-        ]);
+        // // Dung Đỗ
+        // \App\Models\User::where('email', '=', 'dungdo@bonnuoclongnhien.com')->delete();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Dung Đỗ',
+        //     'email' => 'dungdo@bonnuoclongnhien.com',
+        //     'password' => \Hash::make('123456@'),
+        // ]);
 
-        // Bằng Đỗ
-        \App\Models\User::where('email', '=', 'bangdo@bonnuoclongnhien.com')->delete();
-        \App\Models\User::factory()->create([
-            'name' => 'Bằng Đỗ',
-            'email' => 'bangdo@bonnuoclongnhien.com',
-            'password' => \Hash::make('123456&'),
-        ]);
+        // // Bằng Đỗ
+        // \App\Models\User::where('email', '=', 'bangdo@bonnuoclongnhien.com')->delete();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Bằng Đỗ',
+        //     'email' => 'bangdo@bonnuoclongnhien.com',
+        //     'password' => \Hash::make('123456&'),
+        // ]);
     }
 }
